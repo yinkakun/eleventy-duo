a2215e2497ad142ac0c889a9bed73cfee534b59b
Austen Lamacraft <austen.lamacraft@gmail.com>
2022-04-26 23:34:27 +0100

    Update .eleventy so configuration options work
    In the object returned by `eleventyConfig` the configuration options `passthroughFileCopy`, `templateFormats`, `htmlTemplateEngine` and `markdownTemplateEngine` were included in the `dir` object giving the directory structure, so were not being picked up by eleventy. I've moved them out to the returned object.

--- .eleventy.js
+++ .eleventy.js
@@ -136,11 +136,11 @@ module.exports = function (eleventyConfig) {
       output: 'public',
       includes: 'includes',
       data: 'data',
-      layouts: 'layouts',
-      passthroughFileCopy: true,
-      templateFormats: ['html', 'njk', 'md'],
-      htmlTemplateEngine: 'njk',
-      markdownTemplateEngine: 'njk',
+      layouts: 'layouts'
     },
+    passthroughFileCopy: true,
+    templateFormats: ['html', 'njk', 'md'],
+    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: 'njk',
   };
 };
