41aa835b8b8d3bc887a48b3c779f75fac1d59b56
3d12 <3d12@users.noreply.github.com>
2024-09-05 03:11:19 -0500

    npm audit fix
    Resolved high severity vulnerability with html-minifier by
    removing it

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,6 @@ const { DateTime } = require('luxon');
 const readingTime = require('eleventy-plugin-reading-time');
 const pluginRss = require('@11ty/eleventy-plugin-rss');
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
-const htmlmin = require('html-minifier')
 const fs = require('fs');
 const path = require('path');
 
@@ -116,18 +115,6 @@ module.exports = function (eleventyConfig) {
       .filter((tag) => {
         return !generalTags.includes(tag);
       });
-  });
-
-  eleventyConfig.addTransform('htmlmin', function(content, outputPath) {
-    if ( outputPath && outputPath.endsWith(".html") && isProd) {
-      return htmlmin.minify(content, {
-        removeComments: true,
-        collapseWhitespace: true,
-        useShortDoctype: true,
-      });
-    }
-
-    return content;
   });
 
   return {
